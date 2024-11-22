74ec1f3cb68fac9c427b834b1fb523fa7fda96f2
Pranavbc1111 <138850251+Pranavbc1111@users.noreply.github.com>
2024-11-22 19:50:55 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -47,7 +47,7 @@ const ShopContextProvider=(props)=>{
 
         if(token){
             try {
-                await axios.post("http://localhost:4000/api/cart/add",{itemId,size},{headers:{token}})
+                await axios.post("https://ecommerce-backend-three-opal.vercel.app/api/cart/add",{itemId,size},{headers:{token}})
             } catch (error) {
                 console.log(error);
                 toast.error(error.message)
@@ -78,7 +78,7 @@ const ShopContextProvider=(props)=>{
 
         if(token){
             try {
-                await axios.post("http://localhost:4000/api/cart/update",{itemId,size,quantity},{headers:{token}})
+                await axios.post("https://ecommerce-backend-three-opal.vercel.app/api/cart/update",{itemId,size,quantity},{headers:{token}})
             } catch (error) {
                 console.log(error);
                 toast.error(error.message)
@@ -123,7 +123,7 @@ const ShopContextProvider=(props)=>{
 
     const getUserCart=async(token)=>{
         try {
-            const response=await axios.post("http://localhost:4000/api/cart/get",{},{headers:{token}})
+            const response=await axios.post("https://ecommerce-backend-three-opal.vercel.app/api/cart/get",{},{headers:{token}})
             if(response.data.success){
                 setCartItems(response.data.cartData)
             }
